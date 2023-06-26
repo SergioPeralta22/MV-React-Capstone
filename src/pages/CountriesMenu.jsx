e982ffbbda7ca04616c71de6efa1c68545eb4b00
Sergio Peralta <seperalta88@gmail.com>
2023-06-26 14:43:25 -0300

    Add styles to countries page

--- src/pages/CountriesMenu.jsx
+++ src/pages/CountriesMenu.jsx
@@ -11,8 +11,8 @@ const CountriesMenu = () => {
 	const countries = useSelector((state) => state.countries.countries);
 
 	useEffect(() => {
-	  dispatch(getCountries());
-    window.scrollTo(0, 0);
+		dispatch(getCountries());
+		window.scrollTo(0, 0);
 	}, [dispatch]);
 	const url = window.location.href;
 	const continent = url.substring(url.lastIndexOf('/') + 1);
@@ -23,36 +23,63 @@ const CountriesMenu = () => {
 	const filteredCountries = countries.filter((country) => country.region === finalContinent);
 	filteredCountries.sort((a, b) => a.name.common.localeCompare(b.name.common));
 
-  const handleClick = (country) => {
-    dispatch(getCountry(country));
-    console.log(country);
+	const handleClick = (country) => {
+		dispatch(getCountry(country));
+		console.log(country);
+	};
+
+  const getBackgroundColorClass = (index) => {
+    const backgroundColorClasses = ['bg-[#4268B1]', 'bg-[#3E60A2]'];
+    const row= Math.floor(index / 2);
+    return backgroundColorClasses[(index+row) % backgroundColorClasses.length];
   };
+  
+
+  
 
 	return (
 		<div>
-			<header>
+			<header className='bg-blue_l flex items-center gap-4 p-4 w-[100vw]'>
 				<img
-          className='map_svg'
+					className='map_svg w-3/5 h-1/5'
 					alt={continent}
 					src={`https://raw.githubusercontent.com/Ginohmk/worldMaps/main/maps/${continent}/vector.svg`}
 				/>
-				<h3 className='capitalize'>{continent}</h3>
+				<h3 className='capitalize text-2xl pr-4'>{continent}</h3>
+
 			</header>
-			<h4>Population</h4>
-			{filteredCountries.map(({ cca2, name, population, latlng }) => (
-				<NavLink key={cca2} to={`${latlng}:${name.common}:${cca2.toLowerCase()}`} onClick={() => handleClick(cca2)}>
-					<div className='itemCountry'>
-						<img
-              className='map_svg'
-							alt={cca2}
-							src={`https://raw.githubusercontent.com/Ginohmk/worldMaps/main/maps/${cca2.toLowerCase()}/vector.svg`}
-						/>
-						<h3 className='uppercase'>{name.common}</h3>
-            <img src="https://api.iconify.design/streamline:interface-arrows-right-circle-1-arrow-keyboard-circle-button-right.svg?color=%23ffffff" alt="go" />
-						<p>{population}</p>
-					</div>
-				</NavLink>
-			))}
+      <div className="bg-[#35548B] p-2">
+				<h4 className="uppercase">Stats by country</h4>
+        </div>
+        <div className="grid grid-cols-2">
+				{filteredCountries.map(({ cca2, name, population, latlng }, index) => (
+
+					<NavLink
+						key={cca2}
+						to={`${latlng}:${name.common}:${cca2.toLowerCase()}`}
+						onClick={() => handleClick(cca2)}
+					>
+						<div className={`h-full w-full p-4 
+          relative ${getBackgroundColorClass(index)}`}>
+							<img
+								className='map_svg h-[60%] mx-auto'
+								alt={cca2}
+								src={`https://raw.githubusercontent.com/Ginohmk/worldMaps/main/maps/${cca2.toLowerCase()}/vector.svg`}
+							/>
+							<img
+              className="absolute top-3 right-3 h-5"
+								src='https://api.iconify.design/streamline:interface-arrows-right-circle-1-arrow-keyboard-circle-button-right.svg?color=%23ffffff'
+								alt='go'
+							/>
+              <div className="absolute right-4">
+							<h3 className='uppercase text-right'>{name.common}</h3>
+							<p className="text-right">{population}</p>
+              </div>
+						</div>
+					</NavLink>
+
+))}
+</div>
 		</div>
 	);
 };
